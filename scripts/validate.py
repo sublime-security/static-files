09f07520a3493d686a508d24287fae2d4337dde6
MSAdministrator <10687261+MSAdministrator@users.noreply.github.com>
2025-06-03 11:44:37 -0500

    Adding robust error output for github

--- scripts/validate.py
+++ scripts/validate.py
@@ -8,8 +8,14 @@ class Validate:
 
     def __init__(self, manifest: str = "manifest.json") -> None:
         if not os.path.exists(manifest):
-            raise ValueError(f"manifest.json '{manifest}' doesn't exist.")
+            err = f"manifest.json '{manifest}' doesn't exist."
+            self.set_output("error", err)
+            raise ValueError(err)
         self.manifest = manifest
+
+    def set_output(self, name, value):
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+            print(f'{name}={value}', file=fh)
 
     def run(self) -> bool:
         data: dict = {}
@@ -17,12 +23,16 @@ class Validate:
             data = json.load(f)
         for item in data.get("lists"):
             if item.get("file") and not os.path.exists(item["file"]):
-                raise ValueError(f"referenced static file does not exist: '{item}'")
+                err = f"referenced static file does not exist: '{item}'"
+                self.set_output("error", err)
+                raise ValueError(err)
             elif item.get("url"):
                 resp = requests.request("GET", item["url"])
                 resp.raise_for_status()
                 if not resp.ok:
-                    raise requests.exceptions.HTTPError(f"unable to retrieve file from URL: '{item['url']}'")
+                    err = f"unable to retrieve file from URL: '{item['url']}'"
+                    self.set_output("error", err)
+                    raise requests.exceptions.HTTPError(err)
 
 
 if __name__ == "__main__":
